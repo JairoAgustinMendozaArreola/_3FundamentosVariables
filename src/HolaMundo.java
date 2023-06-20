21a31a451a058aafc2497487c50a4dd097c1f8c0
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-06-20 12:59:11 -0600

    12. Ejercicio: Concatenación con Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,31 +1,17 @@
 //Mi clase en Java
 public class HolaMundo {
     public static void main(String[] args) {
-        //Definimos la variable
-        int miVariableEntera = 10;
-        System.out.println(miVariableEntera);
-        //Modificamos el valor de la variable
-        miVariableEntera = 5;
-        System.out.println(miVariableEntera);
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
 
-        String miVariableCadena = "Saludos";
-        System.out.println(miVariableCadena);
+        var i = 3;
+        var j = 4;
 
-        miVariableCadena = "Adios";
-        System.out.println(miVariableCadena);
-
-        //var - Inferencia de tipos en Java
-        var miVariableEntera2 = 15;
-        System.out.println(miVariableEntera2);
-
-        var miVariableCadena2 = "Otra cadena";
-        System.out.println("miVariableCadena2 = " + miVariableCadena2);
-
-        //Valores permitidos
-        var miVariable = 1;
-        var _miVariable = 2;
-        var $miVariable = 3;
-        //var áVariable =10; No se recomienda utilizar
-        //var #miVariable = 2; no se permite utilizar caracteres especiales
+        System.out.println(i + j);//se realiza la suma de numeros
+        System.out.println(i + j + usuario); //Evaluación de izq a  der, realiza suma
+        System.out.println(usuario + i + j); //contexto cadena, todo es cadena
+        System.out.println(usuario + (i + j));//uso de parentesis modifican la prioridad en la evaluacion
     }
 }
