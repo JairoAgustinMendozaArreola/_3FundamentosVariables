81412989256cf2e0e8ce7a45c8be8de7c668042b
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-06-20 13:22:06 -0600

    13. Ejercicio: Caracteres Especiales con Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,17 +1,12 @@
 //Mi clase en Java
 public class HolaMundo {
     public static void main(String[] args) {
-        var usuario = "Juan";
-        var titulo = "Ingeniero";
-        var union = titulo + " " + usuario;
-        System.out.println("union = " + union);
+        var nombre = "Karla";
 
-        var i = 3;
-        var j = 4;
-
-        System.out.println(i + j);//se realiza la suma de numeros
-        System.out.println(i + j + usuario); //Evaluación de izq a  der, realiza suma
-        System.out.println(usuario + i + j); //contexto cadena, todo es cadena
-        System.out.println(usuario + (i + j));//uso de parentesis modifican la prioridad en la evaluacion
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
     }
 }
