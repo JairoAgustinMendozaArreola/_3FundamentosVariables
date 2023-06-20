92e890041bce9c43bd18677ff2be0c87fc4d5435
JairoAgustinMendozaArreola <jairoa.mendozaa@gmail.com>
2023-06-20 14:52:00 -0600

    17. Tipos Numéricos Enteros

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -11,5 +11,20 @@ public class HolaMundo {
         System.out.println("Valor byte: " + numeroByte);
         System.out.println("valor minimo byte: " + Byte.MIN_VALUE);
         System.out.println("valor maximo byte: " + Byte.MAX_VALUE);
+
+        short numeroShort = (short) 32768;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("valor minimo short: " +  Short.MIN_VALUE);
+        System.out.println("valor maximo short: " +  Short.MAX_VALUE);
+
+        int numeroInt = (int) 2147483648L;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("valor minimo int: " +  Integer.MIN_VALUE);
+        System.out.println("valor maximo int: " +  Integer.MAX_VALUE);
+
+        long numeroLong =  9223372036854775807L;
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("valor minimo long: " +  Long.MIN_VALUE);
+        System.out.println("valor maximo long: " +  Long.MAX_VALUE);
     }
 }
